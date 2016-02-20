6db844301c2733daff497ee11b223ffe5eb70daa
Carlos Pinto <pinto.carlosd@gmail.com>
2016-02-20 18:55:45 -0500

    Carlos - Modified implementation to leverage cached image urls to prevent making a request ever x seconds but only when images run out. Added placeholder image.

--- src/scripts/FlickerImageComponent.js
+++ src/scripts/FlickerImageComponent.js
@@ -1,32 +1,63 @@
 var RandomFlickerImage = function(params) {
-    this.pollingInterval = params.pollingInterval;
+    var self = this;
+
+    this.pollingInterval = ko.observable(params.pollingSecondsInterval*1000 || 5000);
     this.imageUrl = ko.observable();
     this.title = ko.observable();
+    this.timer = null;
+    this.imageUrls = [];
 
-    this.retrieveImage();
+    self.initialize();
 };
 
 RandomFlickerImage.prototype = {
-    'retrieveImage': function() {
+    'initialize': function() {
+        this.imageUrl(this.getNextAvailableImageUrl());
+        this.startTimer();
+    },
+    'startTimer': function() {
+        var self = this;
+
+        this.timer = setInterval(function() { 
+            self.imageUrl(self.getNextAvailableImageUrl()); 
+        }, self.pollingInterval());
+    },
+    'resetTimer': function() {
+        clearTimeout(this.timer);
+    },
+    'retrieveImages': function() {
         var self = this;
         var requestUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=1b32f0e9fbf2d2ce345e5c736fa1dc75&format=json&nojsoncallback=1";
 
         $.getJSON(requestUrl, function(response) {
-            var singleImage = self.getSingleImage(response);
-            var imageUrl = self.getImageUrl(singleImage); 
-            self.imageUrl(imageUrl);
+            self.processImageData(response);
         })
         .fail(function() {
-            console.log("image retrieve failed, using placeholder image");
-            self.imageUrl = "someimage.jpg";
+            console.log("image retrieve failed! Check API flicker Key.");
         });
     },
-    'getSingleImage': function(flickerResponse) {
-        if(flickerResponse && flickerResponse.photos && Array.isArray(flickerResponse.photos.photo)) {
-            return flickerResponse.photos.photo[0];
+    'processImageData': function(imageData) {
+        this.imageUrls = [];
+
+        if(imageData && imageData.photos && Array.isArray(imageData.photos.photo)) {
+            var rawImageData =  imageData.photos.photo;
+            for(var i =0; i<rawImageData.length; i++) {
+                this.imageUrls.push(this.getImageUrl(rawImageData[i]));
+            }
         }
 
-        return null;
+        this.imageUrls.push("placeholder.jpg");
+    },
+    'getNextAvailableImageUrl' : function() {
+        var imageUrl = "placeholder.jpg";
+        if(this.imageUrls.length) {
+            imageUrl = this.imageUrls.splice(0,1);
+        }
+        else {
+            this.retrieveImages();
+        }
+
+        return imageUrl;
     },
     'getImageUrl': function(image) {
         var imageUrl = [
